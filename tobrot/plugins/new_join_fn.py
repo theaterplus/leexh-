5caf15e07629179990f9a4833e7c56339fcd79dd
Jagdeep gill [jatt life thuglife] <64634071+patwari6862@users.noreply.github.com>
2020-09-17 15:49:01 +0530

    edit by ne

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @GbotStoreSupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text(""" """, disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
@@ -46,12 +46,12 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/filmyflx/1"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "please use @thugliferenamebot",
         quote=True,
         reply_markup=reply_markup
     )
