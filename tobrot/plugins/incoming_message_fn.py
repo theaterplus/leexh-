11a0ba46d0ac47923abcab28d2d8483bc0969530
Jagdeep gill [jatt life thuglife] <64634071+patwari6862@users.noreply.github.com>
2020-09-17 19:31:48 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -30,7 +30,6 @@ from tobrot.helper_funcs.display_progress import progress_for_pyrogram
 from tobrot.helper_funcs.youtube_dl_extractor import extract_youtube_dl_formats
 from tobrot.helper_funcs.admin_check import AdminCheck
 from tobrot.helper_funcs.ytplaylist import yt_playlist_downg
-from tobrot.helper_funcs.cloneHelper import CloneHelper
 
 async def incoming_purge_message_f(client, message):
     """/purge command"""
@@ -45,9 +44,7 @@ async def incoming_purge_message_f(client, message):
 
 async def incoming_message_f(client, message):
     """/leech command"""
-    #g_id = message.from_user.id
-    #credit = await message.reply_text(f" <a href='tg://user?id={g_id}'></a>", parse_mode="html")
-    i_m_sefg = await credit.reply_text("processing", quote=True)
+    i_m_sefg = await message.reply_text("processing", quote=True)
     is_zip = False
     is_unzip = False
     is_unrar = False
@@ -103,79 +100,16 @@ async def incoming_message_f(client, message):
             f"<b>API Error</b>: {cf_name}"
         )
 #
-async def incoming_gdrive_message_f(client, message):
-    """/gleech command"""
-    #g_id = message.from_user.id
-    #credit = await message.reply_text(f"🧲 Leeching for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="html")
-    i_m_sefg = await credit.reply_text("processing", quote=True)
-    is_zip = False
-    is_unzip = False
-    is_unrar = False
-    is_untar = False
-    if len(message.command) > 1:
-        if message.command[1] == "archive":
-            is_zip = True
-        elif message.command[1] == "unzip":
-            is_unzip = True
-        elif message.command[1] == "unrar":
-            is_unrar = True
-        elif message.command[1] == "untar":
-            is_untar = True
-    # get link from the incoming message
-    dl_url, cf_name, _, _ = await extract_link(message.reply_to_message, "GLEECH")
-    LOGGER.info(dl_url)
-    LOGGER.info(cf_name)
-    if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        # start the aria2c daemon
-        aria_i_p = await aria_start()
-        LOGGER.info(aria_i_p)
-        current_user_id = message.from_user.id
-        # create an unique directory
-        new_download_location = os.path.join(
-            DOWNLOAD_LOCATION,
-            str(current_user_id),
-            str(time.time())
-        )
-        # create download directory, if not exist
-        if not os.path.isdir(new_download_location):
-            os.makedirs(new_download_location)
-        await i_m_sefg.edit_text("trying to download")
-        # try to download the "link"
-        await call_apropriate_function_g(
-            aria_i_p,
-            dl_url,
-            new_download_location,
-            i_m_sefg,
-            is_zip,
-            cf_name,
-            is_unzip,
-            is_unrar,
-            is_untar,
-            message
-        )
-    else:
-        await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
-            f"<b>API Error</b>: {cf_name}"
-        )
-
 
 async def incoming_youtube_dl_f(client, message):
     """ /ytdl command """
-    #g_id = message.from_user.id
-    #credit = await message.reply_text(f"💀 Downloading for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="html")
-    i_m_sefg = await credit.reply_text("processing", quote=True)
+    i_m_sefg = await message.reply_text("**ProcessinG..**", quote=True)
     # LOGGER.info(message)
     # extract link from message
     dl_url, cf_name, yt_dl_user_name, yt_dl_pass_word = await extract_link(
         message.reply_to_message, "YTDL"
     )
     LOGGER.info(dl_url)
-    #if len(message.command) > 1:
-        #if message.command[1] == "gdrive":
-            #with open('blame_my_knowledge.txt', 'w+') as gg:
-                #gg.write("I am noob and don't know what to do that's why I have did this")
     LOGGER.info(cf_name)
     if dl_url is not None:
         await i_m_sefg.edit_text("extracting links")
@@ -188,19 +122,14 @@ async def incoming_youtube_dl_f(client, message):
         # list the formats, and display in button markup formats
         thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
             dl_url,
-            cf_name,
+            # cf_name,
             yt_dl_user_name,
             yt_dl_pass_word,
             user_working_dir
         )
-        print(thumb_image)
-        req = requests.get(f"{thumb_image}")
-        gau_tam = f"{current_user_id}.jpg"
-        open(gau_tam, 'wb').write(req.content)
         if thumb_image is not None:
             await message.reply_photo(
-                #text_message,
-                photo=gau_tam,
+                photo=thumb_image,
                 quote=True,
                 caption=text_message,
                 reply_markup=reply_markup
@@ -216,34 +145,15 @@ async def incoming_youtube_dl_f(client, message):
             "**FCUK**! wat have you entered. \nPlease read /help \n"
             f"<b>API Error</b>: {cf_name}"
         )
+
 #playlist
 async def g_yt_playlist(client, message):
     """ /pytdl command """
-    #i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
     usr_id = message.from_user.id
     G_DRIVE = False
-    if len(message.command) > 1:
-        if message.command[1] == "gdrive":
-            G_DRIVE = True
     if 'youtube.com/playlist' in message.reply_to_message.text:
-        i_m_sefg = await message.reply_text("Downloading...you should wait🤗", quote=True)
+        i_m_sefg = await message.reply_text("Downloading all videos from given playlist. Please wait! 🥱", quote=True)
         await yt_playlist_downg(message.reply_to_message, i_m_sefg, G_DRIVE)
     
     else:
-        await message.reply_text("Reply to youtube playlist link only 🙄")
-        
- #
-async def g_clonee(client, message):
-    """ /gclone command """
-    g_id = message.from_user.id
-    if message.reply_to_message is not None:
-        LOGGER.info(message.reply_to_message.text)
-        gclone = CloneHelper(message)
-        gclone.config()
-        a, h = gclone.get_id()
-        LOGGER.info(a)
-        LOGGER.info(h)
-        await gclone.gcl()
-        await gclone.link_gen_size()
-    else:
-        await message.reply_text("You should reply to a message, which format should be [ID of Gdrive file/folder Name of the file/folder]\nOr read Github for detailled information")
+        await message.reply_text("This command works for youtube playlists only!!")
