9c569f330c113dc3b86e109c2ce0efe3f4359172
Jagdeep gill [jatt life thuglife] <64634071+patwari6862@users.noreply.github.com>
2020-09-17 20:34:12 +0530

    Update custom_thumbnail.py

--- tobrot/plugins/custom_thumbnail.py
+++ tobrot/plugins/custom_thumbnail.py
@@ -19,7 +19,7 @@ async def save_thumb_nail(client, message):
         thumbnail_location,
         str(message.from_user.id) + ".jpg"
     )
-    ismgs = await message.reply_text("processing ...")
+    ismgs = await message.reply_text(" ")
     if message.reply_to_message is not None:
         if not os.path.isdir(thumbnail_location):
             os.makedirs(thumbnail_location)
@@ -44,11 +44,11 @@ async def save_thumb_nail(client, message):
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
-            "Custom video / file thumbnail saved. " + \
-            "This image will be used in the upload, till /clearthumbnail."
+            ""
+            ""
         )
     else:
-        await message.edit("Reply to a photo to save custom thumbnail")
+        await message.edit(" ")
 
 
 async def clear_thumb_nail(client, message):
@@ -60,7 +60,7 @@ async def clear_thumb_nail(client, message):
         thumbnail_location,
         str(message.from_user.id) + ".jpg"
     )
-    ismgs = await message.reply_text("processing ...")
+    ismgs = await message.reply_text("")
     if os.path.exists(thumb_image_path):
         os.remove(thumb_image_path)
-    await ismgs.edit("✅ Custom thumbnail cleared succesfully.")
+    await ismgs.edit("")
